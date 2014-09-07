2e0a5968f5a48c100a69324ec08f50ef9282e177
kou_yeung <kou_yeung@hotmail.com>
2014-09-07 17:46:06 +0900

    テスト追加

---
+++ typings/Kii/Kii-test.ts
@@ -0,0 +1,19 @@
+﻿/// <reference path="Kii.d.ts" />
+
+function main() {
+    // initialize the Kii SDK
+    Kii.initializeWithSite("AppID", "AppKey", KiiSite.JP);
+    var username = "user_123";
+    var password = "123ABC";
+    // auth
+    KiiUser.authenticate(username, password, {
+        success: function (user: KiiUser) {
+            console.log("User authenticated!");
+            console.log(user);
+        },
+        failure: function (user: KiiUser, errStr: string) {
+            console.log("Error authenticating: " + errStr);
+        }
+    });
+}
+
